281ce0a8891cdcc9e9491f6108ff606aa5e4185c
Walter Kleinmaier <16561@edu.szf.at>
2023-10-19 06:41:18 +0200

    Änderungen der Methoden in der Klasse Person.

--- KMS_01_06_LE_02_04/Menu.cs
+++ KMS_01_06_LE_02_04/Menu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace KMS_01_06_LE_02_04
 {
@@ -7,38 +8,32 @@ namespace KMS_01_06_LE_02_04
         public static void Selects()
         {
             int select = 0;
-
             do
             {
                 Console.WriteLine("Anlegen einer Person, drücken Sie die (1): ");
                 Console.WriteLine("Alle Eintäge anzeigen, drücken Sie die (2): ");
                 Console.WriteLine("Anzeigen nach dem Alter, drücken Sie die (3): ");
                 Console.WriteLine("Beenden des Programmnes, drücken Sie die (0): ");
-                Console.WriteLine("Alle Eintäge anzeigen, drücken Sie die (3): ");
                 try
                 {
                     select = Convert.ToInt32(Console.ReadLine());
-
                 }
                 catch (FormatException)
                 {
                     Console.WriteLine("Ungültige Eingabe!");
                     continue;
                 }
-    
                 switch (select)
                 {
                     case 1:
                         string firstName = SelectInput.GetStringInput("Bitte geben Sie den Vornamen ein: ");
                         string lastName = SelectInput.GetStringInput("Bitte geben Sie den Nachnamen ein: ");
                         int age = SelectInput.GetIntInput("Bitte geben Sie das Alter ein: ");
-                        string telephoneNumber = SelectInput.GetStringInput("Bitte geben Sie die Telefonnummer ein: ");
+                        int telephoneNumber = SelectInput.GetIntInput("Bitte geben Sie die Telefonnummer ein: ");
                         var person = new Person(firstName, lastName, age, telephoneNumber);
-                 
                         break;
-
                     case 2:
-                        Person.ShowAllPersons();
+                        Person.ShowAllPersons(ageOf: 0, ageTo: 99);
                         break;
                     case 3:
                         DisplaySubMenu();
@@ -50,42 +45,36 @@ namespace KMS_01_06_LE_02_04
                         Console.WriteLine("Falsche Eingabe");
                         continue;
                 }
-
             } while (true);
         }
         public static void DisplaySubMenu()
         {
             int selectSubMenu = 0;
-
             do
             {
-                Console.WriteLine("Alle Personen unter 18 Jahren, drücken Sie die (1): ");
-                Console.WriteLine("Alle Personen über 30 Jahren, drücken Sie die (2): ");
-                Console.WriteLine("Alle Person über 45 Jahren, drücken Sie die (3): ");
+                Console.WriteLine("Alle Personen unter 18 Jahren bis 29 drücken Sie die (1): ");
+                Console.WriteLine("Alle Personen über 30 Jahren bis 44 drücken Sie die (2): ");
+                Console.WriteLine("Alle Person über 45 Jahren bis 60 drücken Sie die (3): ");
                 Console.WriteLine("Zurück zum Hauptmenü, drücken Sie die (0): ");
-
                 try
                 {
                     selectSubMenu = Convert.ToInt32(Console.ReadLine());
-
-                }catch(FormatException)
+                }
+                catch (FormatException)
                 {
                     Console.WriteLine("Ungültige Eingabe!");
                     continue;
                 }
-                switch(selectSubMenu)
-                {
+                switch (selectSubMenu)
+                {     
                     case 1:
-                        Person.ShowAllUnderEightTeenYearsPersons();
+                        Person.ShowAllPersons(ageOf: 18, ageTo:29);
                         break;
-
                     case 2:
-                        Person.ShowAllUnderThirtyYearsPerson();
-
+                        Person.ShowAllPersons(ageOf: 30, ageTo: 44);
                         break;
                     case 3:
-                        Person.ShowAllUnderFourtyFifeYearsPersons();
-
+                        Person.ShowAllPersons(ageOf: 45, ageTo: 60);
                         break;
                     case 0:
                         Console.WriteLine("Zurück zum Hauptmenü");
